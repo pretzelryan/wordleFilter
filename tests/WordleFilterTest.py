3a91a9b004605f0708105361f2ef0ec8b65a4c9c
pretzelryan <muetzelryan@gmail.com>
2025-01-27 09:36:27 -0600

    Added tests for WordleFilter class.

--- tests/WordleFilterTest.py
+++ tests/WordleFilterTest.py
@@ -1,5 +1,7 @@
 """
-Test file for WordleGuess class.
+Test file for WordleFilter class.
+
+Functionality of the WordleFilter is dependent upon a functioning WordleGuess class.
 """
 
 # Standard imports.
@@ -8,54 +10,39 @@ import unittest
 # Package imports.
 from wordle_filter.WordleGuess import LetterColor
 from wordle_filter.WordleGuess import WordleGuess
+from wordle_filter.WordleFilter import WordleFilter
 
 
-class GuessTest(unittest.TestCase):
-    def test_enumeration_constructor(self):
+class FilterTest(unittest.TestCase):
+    def test_filter_constructor(self):
         """
-        Test the LetterColor enumeration constructor.
+        Test the filter constructor.
+
+        :return:
+        """
+        new_filter = WordleFilter()
+
+        self.assertIsNotNone(new_filter, "WordleFilter failed to initialize.")
+        self.assertIsNotNone(new_filter.remaining_word_list, "WordleFilter remaining word list failed to initialize.")
+        self.assertIsNotNone(new_filter.guess_list, "WordleFilter guess list failed to initialize.")
+
+        self.assertGreater(len(new_filter.remaining_word_list), 1, "WordleFilter initialized remaining word "
+                                                                   "list to an incorrect size.")
+        self.assertEqual(len(new_filter.guess_list), 0, "WordleFilter initialized guess list to incorrect size.")
+
+    def test_filter_perfect(self):
+        """
+        Test the filter function by adding a word with only one green letter.
 
         :return: None
         """
-        self.assertEqual(LetterColor(0).name, "GREEN", "Green enumeration name did not return 'GREEN'")
-        self.assertEqual(LetterColor(1).name, "YELLOW", "Green enumeration name did not return 'YELLOW'")
-        self.assertEqual(LetterColor(2).name, "GREY", "Green enumeration name did not return 'GREY'")
-
-    def test_guess_constructor_normal(self):
-        """
-        Test the WordleGuess constructor as intended to be used.
-
-        :return: None
-        """
-        word = "RATIO"
-
-        int_color_list = [1, 1, 2, 2, 2]
+        word = "SHARK"
+        int_color_list = [0, 0, 0, 0, 0]
         enumeration_list = [LetterColor(num) for num in int_color_list]
 
         new_guess = WordleGuess(word, enumeration_list)
+        new_filter = WordleFilter()
 
-        self.assertIsNotNone(new_guess, "WordleGuess did not initialize.")
-        self.assertEqual(new_guess.word, "ratio", "WordleGuess assigned incorrect word.")
-        self.assertEqual(new_guess.color_list, enumeration_list, "WordleGuess assigned incorrect enumeration list.")
+        new_filter.add_guess(new_guess)
 
-        location_dict = {LetterColor(0): [],
-                         LetterColor(1): [0, 1],
-                         LetterColor(2): [2, 3, 4]}
-
-        self.assertEqual(new_guess.location_dict, location_dict, "Location dictionary assigned incorrectly.")
-
-    def test_guess_constructor_error(self):
-        """
-        Test the WordleGuess constructor for when an error should be raised.
-
-        :return: None
-        """
-
-        word = "BEST"
-
-        int_color_list = [1, 1, 0, 0]
-        enumeration_list = [LetterColor(num) for num in int_color_list]
-
-        # Attempt to use illegal arguments for the WordleGuess constructor (length 4)
-        with self.assertRaises(ValueError):
-            WordleGuess(word, enumeration_list)
+        self.assertEqual(["shark"], new_filter.remaining_word_list, "Filter did not find correct word.")
