44d4242b34fe980f9d7548ba4db9b087755343d5
@pretzelryan <bumpyagents6@gmail.com>
2025-07-30 22:48:01 -0400

    refactored to use Word class.

--- wordle_filter/WordleGuess.py
+++ wordle_filter/WordleGuess.py
@@ -10,8 +10,9 @@
 from enum import Enum
 
 
-# Global Variables
-LETTERS_IN_WORD = 5
+# Project Imports
+from .Word import Word
+from .Word import LETTERS_IN_WORD
 
 
 class LetterColor(Enum):
@@ -29,29 +30,34 @@ class WordleGuess:
     Handles data storage for each individual guessing round.
 
     """
-    def __init__(self, word: str, color_list: list[LetterColor]):
+    def __init__(self, word: Word, color_list: list[LetterColor]):
         """
-        Constructor. Arguments must be length of 5, otherwise raises ValueError.
+        Constructor. List must be of length 5, otherwise raises value error.
 
-        :param word: five-letter word used in guess.
-        :param color_list: List of letter color enumerations in guess.
+        :param word: word used in guess
+        :type word: Word
+        :param color_list: list of LetterColor enumerations, must be length 5.
+        :type color_list: list[LetterColor]
         """
-        if len(word) != LETTERS_IN_WORD or len(color_list) != LETTERS_IN_WORD:
-            raise ValueError("Guess arguments must be of length " + str(LETTERS_IN_WORD) + ".")
+        if not isinstance(word, Word):
+            raise ValueError(f"Word must be of type Word. Got type {type(word)}.")
+        elif len(color_list) != LETTERS_IN_WORD:
+            raise ValueError(f"List must be of length 5. Got length {len(color_list)}.")
 
-        self.word = word.lower()
+        self.word = word
         self.color_list = color_list
 
-        # determine where green, yellow, and grey letters are located to simplify the filtering function.
+        # determine where green, yellow, and grey letters are located. This simplifies the filtering function.
         self.location_dict = self._create_location_dict()
 
-    def _create_location_dict(self):
+    def _create_location_dict(self) -> dict[LetterColor, list[int]]:
         """
         Creates a dictionary that describes the location of colors within the guess.
 
         :return: Dictionary of length three, with elements "green", "yellow", and "grey" enumeration corresponding
         to integer lists that describe the locations of the colored elements.
         """
+        # Decision to format color_list into dict to make the filtering function simpler.
         # example dict = {green: [1, 2], yellow: [4], grey: [3, 5]}
         location_dict = {LetterColor(0): [],
                          LetterColor(1): [],
@@ -62,3 +68,12 @@ class WordleGuess:
             location_dict[self.color_list[i]].append(i)
 
         return location_dict
+
+    def __repr__(self):
+        """
+        String representation of the guess object.
+
+        :return: word and color list
+        :rtype: str
+        """
+        return f"WordleGuess(word={self.word.get_string()}, colors={self.color_list})"
